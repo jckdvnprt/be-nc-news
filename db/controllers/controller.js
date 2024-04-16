c67e59cefa6422bb5ee7dfb5e6d8894de0d19f90
Jack Davenport <jckwdvnprt@gmail.com>
2024-04-16 10:35:59 +0100

    Finished task 4, adding the get articles by id endpoint, created a getArticle function in controller, a fetchArticleFromDatabase function in the model, added TDD tests for 200 and 404 status codes, added the endpoint to the json file, and tidied up the api/get tests

--- db/controllers/controller.js
+++ db/controllers/controller.js
@@ -1,15 +1,33 @@
 const db = require("../connection");
-const fetchTopicsFromDatabase = require("../models/model");
+const {
+  fetchArticleFromDatabase,
+  fetchTopicsFromDatabase,
+} = require("../models/model");
 
 const getTopics = (req, res, next) => {
   fetchTopicsFromDatabase()
     .then((topics) => {
-      res.status(200);
-      res.send(topics);
+      res.status(200).send(topics);
     })
     .catch((error) => {
       res.status(500).send({ msg: "Internal Server Error" });
     });
 };
 
-module.exports = getTopics;
+const getArticle = (req, res) => {
+  const articleId = req.params.article_id;
+  fetchArticleFromDatabase(articleId)
+    .then((article) => {
+      if (article) {
+        res.status(200).send({ article });
+      } else {
+        res.status(404).send({ msg: "Article not found" });
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching article:", error);
+      res.status(500).send({ msg: "Internal Server Error" });
+    });
+};
+
+module.exports = { getTopics, getArticle };
--- db/models/model.js
+++ db/models/model.js
@@ -1,11 +1,34 @@
 const db = require("../connection");
+// const { articleData } = require("../data/test-data");
 
 const fetchTopicsFromDatabase = () => {
   return new Promise((resolve, reject) => {
-    db.query("SELECT * FROM topics;").then((queryResult) => {
-      resolve(queryResult.rows);
-    });
+    db.query("SELECT * FROM topics;")
+      .then((queryResult) => {
+        resolve(queryResult.rows);
+      })
+      .catch((error) => {
+        reject(error);
+      });
   });
 };
 
-module.exports = fetchTopicsFromDatabase;
+function fetchArticleFromDatabase(articleId) {
+  return new Promise((resolve, reject) => {
+    db.query("SELECT * FROM articles WHERE article_id=$1;", [articleId])
+      .then((queryResult) => {
+        if (queryResult.rows.length > 0) {
+          resolve(queryResult.rows);
+        } else {
+          console.log("No article found with articleId:", articleId);
+          resolve(null);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching article:", error);
+        reject(error);
+      });
+  });
+}
+
+module.exports = { fetchTopicsFromDatabase, fetchArticleFromDatabase };
